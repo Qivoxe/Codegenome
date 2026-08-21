77485659daa28b8e553532bc3a53926559d4ccc6
unknown <shivamthechad@gmail.com>
2026-08-21 19:41:29 +0530

    error

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://codegenome-81h1hixre-shivamroy.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
